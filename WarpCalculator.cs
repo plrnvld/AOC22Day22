a516c4fb2cdf72cdca664e55e975365b8add1300
Paul Rijneveld <paulrijneveld@gmail.com>
2023-02-24 19:51:57 +0000

    Warping refactoring

--- WarpCalculator.cs
+++ WarpCalculator.cs
@@ -16,10 +16,10 @@ public class WarpCalculator
         this.boardDict = boardDict;
         this.blockSize = blockSize;
 
-        warpTranslator = InitWarpTranslator();
+        warpTranslator = blockSize == 4 ? InitExampleWarpTranslator() : InitInputWarpTranslator();
     }
 
-    Dictionary<BlockPos, BlockSide> InitWarpTranslator()
+    Dictionary<BlockPos, BlockSide> InitExampleWarpTranslator()
     {
         return new Dictionary<BlockPos, BlockSide>
         {
@@ -43,80 +43,119 @@ public class WarpCalculator
         };
     }
 
+    Dictionary<BlockPos, BlockSide> InitInputWarpTranslator()
+    {
+        return new Dictionary<BlockPos, BlockSide>
+        {
+            /*
+            [new BlockPos(3, 1, Facing.Right)] = new BlockSide(3, 1, Side.Left),
+            [new BlockPos(3, 2, Facing.Right)] = new BlockSide(1, 2, Side.Left),
+            [new BlockPos(4, 3, Facing.Right)] = new BlockSide(3, 3, Side.Left),
+
+            [new BlockPos(3, 1, Facing.Left)] = new BlockSide(3, 1, Side.Right),
+            [new BlockPos(1, 2, Facing.Left)] = new BlockSide(3, 2, Side.Right),
+            [new BlockPos(3, 3, Facing.Left)] = new BlockSide(4, 3, Side.Right),
+
+            [new BlockPos(1, 2, Facing.Down)] = new BlockSide(1, 2, Side.Up),
+            [new BlockPos(2, 2, Facing.Down)] = new BlockSide(2, 2, Side.Up),
+            [new BlockPos(3, 3, Facing.Down)] = new BlockSide(3, 1, Side.Up),
+            [new BlockPos(4, 3, Facing.Down)] = new BlockSide(4, 3, Side.Up),
+
+            [new BlockPos(1, 2, Facing.Up)] = new BlockSide(1, 2, Side.Down),
+            [new BlockPos(2, 2, Facing.Up)] = new BlockSide(2, 2, Side.Down),
+            [new BlockPos(3, 1, Facing.Up)] = new BlockSide(3, 3, Side.Down),
+            [new BlockPos(4, 3, Facing.Up)] = new BlockSide(4, 3, Side.Down),
+            */
+        };
+    }
+
     public Position GetWarpTarget(Position curr)
     {
+        Console.WriteLine($"---> Calculating warp target from {curr}");
+        
         var xBlock = (int)Math.Ceiling(((double)curr.Tile.X) / blockSize);
         var yBlock = (int)Math.Ceiling(((double)curr.Tile.Y) / blockSize);
-        
+        var blockCol = RelPos(curr.Tile.X);
+        var blockRow = RelPos(curr.Tile.Y);
+        var blockColInv = blockSize + 1 - blockCol;
+        var blockRowInv = blockSize + 1 - blockRow;
+
+        Console.WriteLine($"---> block x={xBlock}, block y={yBlock}");
         var targetBlockSide = warpTranslator[new BlockPos(xBlock, yBlock, curr.Facing)];
 
+        var addX = (targetBlockSide.XBlock - 1) * blockSize;
+        var addY = (targetBlockSide.YBlock - 1) * blockSize;
+        
+
         var newFacing = FromSide(targetBlockSide.Side);
-        var newCol = targetBlocSide.XBlock // #######################
-        
-        int BlockStart(int block) => blockSize * (block - 1) + 1;
-        int BlockEnd(int block) => blockSize * block;
-        
-        var facing = curr.Facing;
-        var col = curr.Tile.X;
-        var row = curr.Tile.Y;        
-        var blockNum = BlockNum(curr.Tile.X, curr.Tile.Y);
 
         var newCol = -1;
         var newRow = -1;
 
-        if (facing is Facing.Right)
+        Console.WriteLine($"---> Aiming at side {targetBlockSide.Side}");
+
+        switch (targetBlockSide.Side)
         {
-            newCol = blockNum switch
-            {
-                1 => BlockStart(3),
-                4 => BlockStart(1),
-                6 => BlockStart(3),
-                _ => throw new Exception("not supported")
-            };
-
-            newRow = row;
+            case Side.Left:
+                newCol = addX + 1;
+                newRow = addY + curr.Facing switch
+                {
+                    Facing.Right => blockRow,
+                    Facing.Up => blockCol,
+                    Facing.Left => blockRowInv,
+                    Facing.Down => blockColInv,
+                    _ => throw new Exception("Unhandled")
+                };
+                break;
+            case Side.Up:
+                newCol = addX + curr.Facing switch
+                {
+                    Facing.Right => blockRowInv,
+                    Facing.Up => blockColInv,
+                    Facing.Left => blockRow,
+                    Facing.Down => blockCol,
+                    _ => throw new Exception("Unhandled")
+                };
+                newRow = addY + 1;
+                break;
+            case Side.Right:
+                newCol = addX + blockSize;
+                newRow = addY + curr.Facing switch
+                {
+                    Facing.Right => blockRowInv,
+                    Facing.Up => blockColInv,
+                    Facing.Left => blockRow,
+                    Facing.Down => blockCol,
+                    _ => throw new Exception("Unhandled")
+                };
+                break;
+            case Side.Down:
+                newCol = addX + curr.Facing switch
+                {
+                    Facing.Right => blockRow,
+                    Facing.Up => blockCol,
+                    Facing.Left => blockRowInv,
+                    Facing.Down => blockColInv,
+                    _ => throw new Exception("Unhandled")
+                };
+                newRow = addY + blockSize;
+                break;
+            default:
+                throw new Exception("Misery");
         }
-        else if (facing is Facing.Left)
-        {
-            newCol = blockNum switch
-            {
-                1 => BlockEnd(3),
-                2 => BlockEnd(3),
-                5 => BlockEnd(4),
-                _ => throw new Exception("not supported")
-            };
 
-            newRow = row;
-        }
-        else if (facing is Facing.Up)
-        {
-            newRow = blockNum switch
-            {
-                1 => BlockEnd(3),
-                2 => BlockEnd(2),
-                3 => BlockEnd(2),
-                6 => BlockEnd(3),
-                _ => throw new Exception("not supported")
-            };
+        var newTile = boardDict[(newCol, newRow)];
+        return new Position(newTile, newFacing);
+    }
 
-            newCol = col;
-        }
-        else // Facing.Down
-        {
-            newRow = blockNum switch
-            {
-                2 => BlockStart(2), 
-                3 => BlockStart(2),
-                5 => BlockStart(1),
-                6 => BlockStart(3),
-                _ => throw new Exception("not supported")
-            };
+    int RelPos(int pos)
+    {
+        var result = pos;
 
-            newCol = col;
-        }
+        while (result > blockSize)
+            result -= blockSize;
 
-        var nextTile = boardDict[(newCol, newRow)];
-        return new Position(nextTile, facing);        
+        return result;        
     }
 
     Facing FromSide(Side side)
@@ -126,7 +165,8 @@ public class WarpCalculator
             Side.Left => Facing.Right,
             Side.Right => Facing.Left,
             Side.Up => Facing.Down,
-            Side.Down => Facing.Up
+            Side.Down => Facing.Up,
+            _ => throw new Exception("No side matches")
         };
     }
 
@@ -145,9 +185,9 @@ public class WarpCalculator
             (2, 3) => 4,
             (3, 3) => 5,
             (3, 4) => 6,
-            _ => throw new Exception($"No match with ({rowBlock},{colBlock}), Input x={x} and y={y} and blockSize={blockSize}")          
+            _ => throw new Exception($"No match with ({rowBlock},{colBlock}), Input x={x} and y={y} and blockSize={blockSize}")
         };
-    }    
+    }
 }
 
 public record BlockPos(int XBlock, int YBlock, Facing Facing);
